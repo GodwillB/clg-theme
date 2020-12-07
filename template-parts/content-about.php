0b0528ad5b7aa0029baa4e23c646da7a862c0672
GodwillB <iamgodwillb@gmail.com>
2020-12-07 17:43:24 +0300

    about-header

---
+++ template-parts/content-about.php
@@ -0,0 +1,18 @@
+<section class="header-about">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-md-12">
+				
+					<h2 class="mx-auto text-center text-white"><?php the_title(); ?></h2>
+				
+			</div>
+
+		</div>
+		<div class="row justify-content-center">
+			<div class="col-md-1">
+				<div class="border-about"></div>
+			</div>
+			
+		</div>
+	</div>
+</section>
