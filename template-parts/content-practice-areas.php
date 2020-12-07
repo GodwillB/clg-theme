c56199701f05b897577b2ed2c49d726747cd583a
GodwillB <iamgodwillb@gmail.com>
2020-12-08 01:30:27 +0300

    prac areas page done

---
+++ template-parts/content-practice-areas.php
@@ -0,0 +1,75 @@
+<section class="header-about">
+	<div class="overlay">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-12">
+					<h2 class="mx-auto text-center text-white"><?php the_title(); ?></h2>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-md-1">
+					<div class="border-about"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="p-4">
+	<div class="container">
+		<div class="row justify-content-center py-4">
+			<div class="col-md-4 align-items-center">
+				<div class="prac-areas-single">
+					  <div class="card-body">
+					    <h5 class="card-title text-center">Capital Markets</h5>
+					    <p class="card-text text-center">Led by our Principal Officer, who has been a legal practitioner in the capital markets for over 14 years, we are ranked as a top law firm in capital markets practice in Kenya by the prestigious Chambers & Partners Global.</p>
+					    <a href="#" class="text-center align-middle" style=" text-align: center; display: block; margin: 0 auto;">Read More
+					    </a>
+					  </div>
+				</div>
+			</div>
+			<div class="col-md-4 align-items-center">
+				<div class="prac-areas-single">
+					  <div class="card-body">
+					    <h5 class="card-title text-center text-capitalize">Mergers and Acquistions</h5>
+					    <p class="card-text text-center">M&A is one of our main practice specializations and we pride in having advised many entrepreneurs and investors in their transactions.</p>
+					    <a href="#" class="text-center align-middle" style=" text-align: center; display: block; margin: 0 auto;">Read More
+					    </a>
+					  </div>
+				</div>
+			</div>
+			<div class="col-md-4 align-items-center">
+				<div class="prac-areas-single">
+					  <div class="card-body">
+					    <h5 class="card-title text-center text-capitalize">Corporate Finance</h5>
+					    <p class="card-text text-center">Relying on specialist knowledge of corporate finance and long experience, we have advised many clients on capital raising transactions through equity, debt and hybrid structures.</p>
+					    <a href="#" class="text-center align-middle" style=" text-align: center; display: block; margin: 0 auto;">Read More
+					    </a>
+					  </div>
+				</div>
+			</div>
+		</div>
+		<div class="row justify-content-center py-4">
+			<div class="col-md-4 align-items-center">
+				<div class="prac-areas-single">
+					  <div class="card-body">
+					    <h5 class="card-title text-center">Property Law</h5>
+					    <p class="card-text text-center">Under our property law practice, we work with top property developers, retailer companies and energy companies in property acquisition and development, financing, and leasing transactions.</p>
+					    <a href="#" class="text-center align-middle" style=" text-align: center; display: block; margin: 0 auto;">Read More
+					    </a>
+					  </div>
+				</div>
+			</div>
+			<div class="col-md-4 align-items-center">
+				<div class="prac-areas-single">
+					  <div class="card-body">
+					    <h5 class="card-title text-center">Corporate Services</h5>
+					    <p class="card-text text-center">Our established and resourced corporate law practice offers the full range of structuring, registration and compliance support services to our clients setting up businesses, trusts </p>
+					    <a href="#" class="text-center align-middle" style=" text-align: center; display: block; margin: 0 auto;">Read More
+					    </a>
+					  </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
