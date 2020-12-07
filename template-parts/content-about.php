49c2be8e848389ef89877602bdd7385f35b00aff
GodwillB <iamgodwillb@gmail.com>
2020-12-07 23:04:48 +0300

    11

--- template-parts/content-about.php
+++ template-parts/content-about.php
@@ -2,17 +2,131 @@
 	<div class="container">
 		<div class="row justify-content-center">
 			<div class="col-md-12">
-				
-					<h2 class="mx-auto text-center text-white"><?php the_title(); ?></h2>
-				
+				<h2 class="mx-auto text-center text-white"><?php the_title(); ?></h2>
 			</div>
-
 		</div>
 		<div class="row justify-content-center">
 			<div class="col-md-1">
 				<div class="border-about"></div>
 			</div>
+		</div>
+	</div>
+</section>
+
+<section class="about-intro">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<h2 class="section-intro">Trusted Legal Services
+					<span class="separator secondary-color"></span>
+
+				</h2>
+				<h3>Mission</h3>
+				<p>To be a top 5 transactional law firm in Kenya by end of 2020.</p>
+			</div>
+			<div class="col-md-8">
+				<div class="row py-4">
+					<div class="col-md-4 p-5">
+						<div class="row">
+							<img src="<?php echo get_bloginfo('template_url') ?>/img/dispute.svg" class="img-fluid">
+						</div>
+						<div class="row justify-content-center"><h2 class="text-center secondary-color">Excellence</h2></div>
+						<div class="row justify-content-center">
+							<p class="text-center ">We hold ourselves to a standard of excellence in all our work.</p>
+						</div>
+					</div>
+					<div class="col-md-4 p-5">
+						<div class="row ">
+							<img src="<?php echo get_bloginfo('template_url') ?>/img/dispute.svg" class="img-fluid">
+						</div>
+						<div class="row justify-content-center"><h2 class="text-center secondary-color">Integrity</h2></div>
+						<div class="row justify-content-center">
+							<p  class="text-center">We treasure our integrity. We also value integrity in our clients.</p>
+						</div>
+					</div>
+					<div class="col-md-4 p-5">
+						<div class="row">
+							<img src="<?php echo get_bloginfo('template_url') ?>/img/dispute.svg" class="img-fluid">
+						</div>
+						<div class="row justify-content-center"><h2 class="text-center secondary-color">Charity</h2></div>
+						<div class="row justify-content-center text-center">
+							<p>We support our colleagues in good and bad times. We also give back to our society.	</p>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section>
+	<div class="container">
+		<div class="intro-position" >
+			<div class="row">
+				<div class="col-md-6">
+					<img src="https://images.unsplash.com/photo-1575505586569-646b2ca898fc?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=905&q=80" class="img-fluid">
+				</div>
+				<div class="col-md-6">
+					<div class="intro-position-text p-4">
+						<h2 class="">The Firm</h2>
+
+						<p>We are a transactional law firm based in Nairobi. Our practice areas are capital markets, corporate finance, mergers and acquisitions, business structuring (domestic and offshore), mining, property and contracts. We serve over half of the investment banks and stockbrokers licensed in Kenya as well as the central capital markets institutions. We also provide services to Fund Managers, PE Funds, independent financial advisors, investors (Kenyan and foreign) and real estate developers. </p>
+
+						<p>We have consistently delivered to our clients’ requirements through comprehensive legal research and proficient service delivery. We highly value our clients and we carefully nurture each relationship that we have. We walk the long journey with our clients
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="mission-about p-5">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-md-6">
+				<h2 class="text-center pt-5 secondary-color">Who We Are</h2>
+				<p class="text-center text-white pt-5">we are a team of dedicated lawyers specializing in capital markets finance, mergersand acquisitions,property law and corporate services. These practice areas are supported by our experience and qualifications.Through this focusedapproach, we have earned the trust of leading corporates and entrepreneurs in Kenya.
+				</p>
+			</div>
+			<div class="col-md-6">
+				<h2 class="text-center pt-5 secondary-color">Our Mission</h2>
+				<p class="text-center text-white pt-5">To be a top 5 transactional law firm in Kenya by end of 2020.</p>
+			</div>
 			
+		</div>
+		<div class="row justify-content-center">
+			<div class="col-md-8">
+				<h2 class="text-center pt-5 secondary-color">Why Choose Us</h2>
+				<p class="text-center text-white pt-5">If you are seeking a focused, thorough and responsive commercial legal team, we will suit your requirement. We are devoted to providing high quality legal representation to discerning clients. Our team led by our Principal Officer, Nicholas J. Gitonga, delivers comprehensive legal research and proficient legal advisory services.</p> 
+
+				<P class="text-white text-center" >We work with a majority of the investment banks and stockbrokers licensed in Kenya as well as fund managers, independent financial advisors, and the central capital markets. Over the years we have supported entrepreneurs and real estate developers to structure successful transactions and provide them with ongoing corporate legal services. For purposes of delivering robust legal services, we also partner with international law firms and corporate service providers.</p>
+			</div>
+		</div>
+
+		<div class="row justify-content-center">
+			
+		</div>
+		
+	</div>
+</section>
+
+<section class="counter-about">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3">
+				
+			</div>
+			<div class="col-md-3">
+				
+			</div>
+			<div class="col-md-3">
+				
+			</div>
+			<div class="col-md-3">
+				
+			</div>
 		</div>
 	</div>
 </section>
